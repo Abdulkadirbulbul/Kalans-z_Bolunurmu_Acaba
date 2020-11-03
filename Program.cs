acb6d591d550afbc458fbdc01f27d5df4c1d2d12
Abdulkadirbulbul <kadirwinchester25@gmail.com>
2020-11-03 18:31:37 +0300

    Bilmem?  denede gör

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace odev_3hafta_uygulama3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Girilen 2 sayının biri birine tam bölünüp bölünemedğini bulan programı yazınız.
+            Console.WriteLine("İlk sayıyı giriniz.");
+            int ilk = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı giriniz.");
+            int iki = Convert.ToInt32(Console.ReadLine());
+            int sonuc = 0;
+            if (ilk > iki)
+            {
+                sonuc = ilk % iki;
+            }
+            else if (iki < ilk)
+            {
+                sonuc = iki % ilk;
+            }
+            else
+            {
+                sonuc = 0;
+            }
+            if (sonuc == 0)
+            {
+                Console.WriteLine("Birbirine kalansız ve tam bölünür.");
+            } else Console.WriteLine("Birbirine tam bölünemez ve kalan= "+sonuc+"  olur.");
+            Console.ReadKey();
+        }
+    }
+}
